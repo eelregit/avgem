4754f6bee52bdca82a273d1c5e30e33102dcda77
Yin Li <eelregit@gmail.com>
2018-08-31 18:17:11 -0700

    Fix description and bump version

--- avgem/__init__.py
+++ avgem/__init__.py
@@ -1,8 +1,6 @@
-"""Average a tabulated function to bins;
-Re-average them into different bins.
-"""
+"""Average a tabulated function to bins; Re-average them into different bins."""
 
 from .avg import avg
 from .reavg import reavg
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
